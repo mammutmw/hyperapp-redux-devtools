a473a848c8f4d74d82af9b81ea7c18af13cc09df
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-06-05 11:18:01 +0200

    Allow async/await functions

--- index.js
+++ index.js
@@ -1,84 +1,76 @@
-var { createStore } = require("redux");
-var { composeWithDevTools } = require("redux-devtools-extension");
+const { createStore } = require('redux')
+const { composeWithDevTools } = require('redux-devtools-extension')
 
-function reduxReducer(state = {}, action) {
-  return Object.assign({}, state, action.payload);
+const reduxReducer = (state = {}, action) => {
+  return Object.assign({}, state, action.payload)
 }
 
-function reducAction(name, data) {
+const reducAction = (name, data) => {
   return {
     type: name,
     payload: data
-  };
+  }
 }
 
-function copy(target, source) {
-  var obj = {};
-  for (var i in target) obj[i] = target[i];
-  for (var i in source) obj[i] = source[i];
-  return obj;
+const copy = (target, source) => {
+  const obj = {}
+  for (const t in target) obj[t] = target[t]
+  for (const s in source) obj[s] = source[s]
+  return obj
 }
 
-function set(path, value, source, target) {
+const set = (path, value, source, target) => {
   if (path.length) {
     target[path[0]] =
-      1 < path.length ? set(path.slice(1), value, source[path[0]], {}) : value;
-    return copy(source, target);
+      path.length > 1 ? set(path.slice(1), value, source[path[0]], {}) : value
+    return copy(source, target)
   }
-  return value;
+  return value
 }
 
-function get(path, source) {
-  for (var i = 0; i < path.length; i++) {
-    source = source[path[i]];
+const get = (path, source) => {
+  for (let i = 0; i < path.length; i++) {
+    source = source[path[i]]
   }
-  return source;
+  return source
 }
 
-module.exports = function devtools(app) {
-  var composeEnhancers = composeWithDevTools({ action: reducAction });
-  var store;
+export default (app) => {
+  const composeEnhancers = composeWithDevTools({ action: reducAction })
 
-  return function(state, actions, view, container) {
-    var appActions;
-
-    function wire(path, actions) {
-      for (var key in actions) {
-        if (typeof actions[key] === "function") {
-          (function(key, action) {
-            actions[key] = function() {
-              var reducer = action.apply(this, arguments);
+  return async (state, actions, view, container) => {
+    const wire = (path, actions) => {
+      for (const key in actions) {
+        if (typeof actions[key] === 'function') {
+          ((key, action) => {
+            actions[key] = async function () {
+              const reducer = await action.apply(this, arguments)
               return function (slice) {
-                var data = typeof reducer === "function"
+                const data = typeof reducer === 'function'
                   ? reducer(slice, get(path, appActions))
-                  : reducer;
+                  : reducer
                 if (data && !data.then) {
-                  state = set(path, copy(slice, data), state, {});
-                  store.dispatch(reducAction(key, state));
+                  state = set(path, copy(slice, data), state, {})
+                  store.dispatch(reducAction(key, state))
                 }
-                return data;
-              };
-            };
-          })(key, actions[key]);
+                return data
+              }
+            }
+          })(key, actions[key])
         } else {
-          wire(path.concat(key), (actions[key] = copy(actions[key])));
+          wire(path.concat(key), (actions[key] = copy(actions[key])))
         }
       }
     }
-    wire([], (actions = copy(actions)));
+    wire([], (actions = copy(actions)))
 
-    actions.replaceState = function(actualState) {
-      return function (state) {
-        return actualState;
-      }
-    };
-    store = createStore(reduxReducer, state, composeEnhancers());
-    store.subscribe(function() {
-      appActions.replaceState(store.getState());
-    });
+    actions.replaceState = (actualState) => (state) => actualState
+    const store = createStore(reduxReducer, state, composeEnhancers())
+    store.subscribe(() => {
+      appActions.replaceState(store.getState())
+    })
+    const appActions = await app(state, actions, view, container)
 
-    appActions = app(state, actions, view, container);
-    return appActions;
-  };
-};
-
+    return appActions
+  }
+}
